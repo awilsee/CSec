93411d6cdf67d40432e7500c010aa7909a76dd3f
Andreas Wilhelm <awilsee@googlemail.com>
2019-01-25 23:47:54 -0800

    some modifications for reuse

--- Lab1/task5.py
+++ Lab1/task5.py
@@ -16,7 +16,8 @@ cipher_key = os.urandom(aes_bytes)
 aes_prim = AES.new(cipher_key)
 
 
-def add_pkcs7(input, num_bytes):
+def add_pkcs7(input):
+    num_bytes = aes_bytes - (len(input) % aes_bytes)
     fil_bytes = bytes()
     for x in range(num_bytes):
         fil_bytes += bytes([num_bytes])
@@ -24,44 +25,47 @@ def add_pkcs7(input, num_bytes):
     return input
 
 
-def encrypt_ecb(file_path):
+def get_padded_file(file_path):
     file = open(file_path, "rb").read()
-    file_length = len(file)
-    print("file {} has {:d} characters".format(file_path, file_length))
+    print("file {} has {:d} characters".format(file_path, len(file)))
 
-    #padding
-    file = add_pkcs7(file, aes_bytes - (file_length % aes_bytes))
+    # padding
+    return add_pkcs7(file)
 
+
+def encrypt_ecb(data):
     x = 0
     file_enc = bytes()
-    while x < len(file) / aes_bytes:
-        file_enc += aes_prim.encrypt(file[x * aes_bytes:((x + 1) * aes_bytes)])
+    while x < len(data) / aes_bytes:
+        file_enc += aes_prim.encrypt(data[x * aes_bytes:((x + 1) * aes_bytes)])
         x += 1
     return file_enc
 
 
-def encrypt_cbc(file_path, init_iv):
-    file = open(file_path, "rb").read()
-    file_length = len(file)
-    print("file {} has {:d} characters".format(file_path, file_length))
+def encrypt_ecb_file(file_path):
+    return encrypt_ecb(get_padded_file(file_path))
 
-    #padding
-    file = add_pkcs7(file, aes_bytes - (file_length % aes_bytes))
 
+def encrypt_cbc(data, init_iv):
     x = 0
     file_enc = bytes()
-    while x < len(file) / aes_bytes:
+    while x < len(data) / aes_bytes:
         if 0 == x:
-            xor_bytes = xorfunc(init_iv, file[x * aes_bytes:((x + 1) * aes_bytes)])
+            xor_bytes = xorfunc(init_iv, data[x * aes_bytes:((x + 1) * aes_bytes)])
         else:
-            xor_bytes = xorfunc(file_enc[(x - 1) * aes_bytes:(x * aes_bytes)], file[x * aes_bytes:((x + 1) * aes_bytes)])
+            xor_bytes = xorfunc(file_enc[(x - 1) * aes_bytes:(x * aes_bytes)], data[x * aes_bytes:((x + 1) * aes_bytes)])
         file_enc += aes_prim.encrypt(xor_bytes)
         x += 1
     return file_enc
 
 
+def encrypt_cbc_file(file_path, init_iv):
+    return encrypt_cbc(get_padded_file(file_path), init_iv)
+
+
+
 if __name__ == '__main__':
-    enc_txt = encrypt_ecb(txtfilePath)
+    enc_txt = encrypt_ecb_file(txtfilePath)
     open(txtfilePath + ending_ecb[:8], "wb").write(enc_txt)
 
     #test decyrpt ecb
@@ -69,14 +73,14 @@ if __name__ == '__main__':
     plaintext = cipher.decrypt(enc_txt)
     print(plaintext)
 
-    enc_bytes = encrypt_ecb(pic1)
+    enc_bytes = encrypt_ecb_file(pic1)
     write_bmp_file(pic1 + ending_ecb, enc_bytes)
 
-    enc_bytes = encrypt_ecb(pic2)
+    enc_bytes = encrypt_ecb_file(pic2)
     write_bmp_file(pic2 + ending_ecb, enc_bytes)
 
     init_iv = os.urandom(aes_bytes)
-    enc_txt = encrypt_cbc(txtfilePath, init_iv)
+    enc_txt = encrypt_cbc_file(txtfilePath, init_iv)
     open(txtfilePath + ending_cbc[:8], "wb").write(enc_txt)
 
     #test decyrpt cbc
@@ -84,8 +88,8 @@ if __name__ == '__main__':
     plaintext = cipher.decrypt(enc_txt)
     print(plaintext)
 
-    enc_bytes = encrypt_cbc(pic1, init_iv)
+    enc_bytes = encrypt_cbc_file(pic1, init_iv)
     write_bmp_file(pic1 + ending_cbc, enc_bytes)
 
-    enc_bytes = encrypt_cbc(pic2, init_iv)
+    enc_bytes = encrypt_cbc_file(pic2, init_iv)
     write_bmp_file(pic2 + ending_cbc, enc_bytes)
