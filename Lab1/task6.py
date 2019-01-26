d922de55dcfe37cc1ea1ffee77d18a096827afbd
Andreas Wilhelm <awilsee@googlemail.com>
2019-01-25 23:51:07 -0800

    t6 - deleted old/unused stuff

--- Lab1/task6.py
+++ Lab1/task6.py
@@ -1,21 +1,10 @@
 import os
 import urllib.parse
 
-from Crypto.Cipher import AES
-
 from task5 import add_pkcs7
 from tasks import encrypt_cbc
 
-txtfilePath = "./txtFile"
-pic1 = "./cp-logo.bmp"
-pic2 = "./mustang.bmp"
-
-ending_ecb = "-enc-ecb.bmp"
-ending_cbc = "-enc-cbc.bmp"
-
 aes_bytes = 16
-cipher_key = os.urandom(aes_bytes)
-aes_prim = AES.new(cipher_key)
 
 init_iv = os.urandom(aes_bytes)
 
@@ -45,36 +34,3 @@ if __name__ == '__main__':
         print("Congrats you're admin!")
     else:
         print("You're not admin with your string!")
-
-
-
-    '''
-    enc_txt = encrypt_ecb(txtfilePath)
-    open(txtfilePath + ending_ecb[:8], "wb").write(enc_txt)
-
-    #test decyrpt ecb
-    cipher = AES.new(cipher_key, AES.MODE_ECB)
-    plaintext = cipher.decrypt(enc_txt)
-    print(plaintext)
-
-    enc_bytes = encrypt_ecb(pic1)
-    write_bmp_file(pic1 + ending_ecb, enc_bytes)
-
-    enc_bytes = encrypt_ecb(pic2)
-    write_bmp_file(pic2 + ending_ecb, enc_bytes)
-
-    init_iv = os.urandom(aes_bytes)
-    enc_txt = encrypt_cbc(txtfilePath, init_iv)
-    open(txtfilePath + ending_cbc[:8], "wb").write(enc_txt)
-
-    #test decyrpt cbc
-    cipher = AES.new(cipher_key, AES.MODE_CBC, init_iv)
-    plaintext = cipher.decrypt(enc_txt)
-    print(plaintext)
-
-    enc_bytes = encrypt_cbc(pic1, init_iv)
-    write_bmp_file(pic1 + ending_cbc, enc_bytes)
-
-    enc_bytes = encrypt_cbc(pic2, init_iv)
-    write_bmp_file(pic2 + ending_cbc, enc_bytes)
-'''
