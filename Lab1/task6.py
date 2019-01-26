8757aa9359b72071b5da2a85d528af4801b97c0b
Andreas Wilhelm <awilsee@googlemail.com>
2019-01-26 00:34:00 -0800

    t6 runs

--- Lab1/task6.py
+++ Lab1/task6.py
@@ -1,8 +1,11 @@
+import base64
+import binascii
 import os
 import urllib.parse
 
 from task5 import add_pkcs7
-from tasks import encrypt_cbc
+from task5 import encrypt_cbc
+from task5 import decrypt_cbc
 
 aes_bytes = 16
 
@@ -12,15 +15,13 @@ init_iv = os.urandom(aes_bytes)
 def submit(data):
     url = "userid=321;userdata=" + data + ";session-id=31337"
     url = urllib.parse.quote(url, safe="\' ',")
-    print("1: " + url)
-    url = add_pkcs7(url)
-    print("2: " + url)
+    url = add_pkcs7(bytes(url, 'utf-8'))
     return encrypt_cbc(url, init_iv)
 
 
 def verify(data):
-    url = "dd"#decrypt_cbc(data)
-    if -1 == url.find(";admin=true;"):
+    url = decrypt_cbc(data, init_iv)
+    if -1 == url.decode().find(";admin=true;"):
         return False
     else:
         return True
@@ -28,9 +29,12 @@ def verify(data):
 
 if __name__ == '__main__':
     txt = "You're the man now, dog"
+    txt = input("Enter string: ")
     print("Entered String: " + txt)
     enc_url = submit(txt)
+    print()
+
     if verify(enc_url):
         print("Congrats you're admin!")
     else:
-        print("You're not admin with your string!")
+        print("You're NOT admin with your string!")
